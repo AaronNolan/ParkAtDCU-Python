9066d3c40d17ee179c0cc99cf074d3f332a697f7
Aaron Nolan <aaron.nolan67@mail.dcu.ie>
2021-01-25 12:38:27 +0000

    Update form.py

--- src/ca377/parkatdcu/form.py
+++ src/ca377/parkatdcu/form.py
@@ -1,12 +1,12 @@
-from django import forms
-from .models import Campus
+# from django import forms
+# from .models import Campus
 
 
-camps = Campus.objects.all()
-campi = []
-for camp in camps:
-    campi.append((camp.name, camp.name))
+# camps = Campus.objects.all()
+# campi = []
+# for camp in camps:
+#     campi.append((camp.name, camp.name))
 
 
-class CarparkForm(forms.Form):
-    campus = forms.ChoiceField(label="campus", choices=campi, widget=forms.Select(attrs={'class': "form-control"}))
+# class CarparkForm(forms.Form):
+#     campus = forms.ChoiceField(label="campus", choices=campi, widget=forms.Select(attrs={'class': "form-control"}))
